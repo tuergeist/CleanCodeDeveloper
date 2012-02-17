bb1beefcba8b8e7bdb00eb0fe7c327af4456919c
Christoph Becker <christoph.becker@rohde-schwarz.com>
2012-02-17 10:16:42 +0100

    NEW: PrimeCheck as maven project added

---
+++ java/PrimeCheck/src/main/java/de/chbecker/ccd/App.java
@@ -0,0 +1,13 @@
+package de.chbecker.ccd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
